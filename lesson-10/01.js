a426283e2d47ccc9e40ba68f8a159e47e5db1200
Daria Mochalova <shichimiya74@gmail.com>
2024-09-16 12:20:37 +0500

    format file

--- lesson-10/01.js
+++ lesson-10/01.js
@@ -24,37 +24,38 @@
 const model = {
   movies: [],
   addMovie(title, description) {
-    const id = Math.random()
-    const newMovie = { id, title, description }
-    this.movies.push(newMovie)
-    view.renderMovies(this.movies)
+    const id = Math.random();
+    const newMovie = { id, title, description };
+    this.movies.push(newMovie);
+    view.renderMovies(this.movies);
   },
-  // your code
-}
+  // your code_________________________________________________________________
+};
 
 const view = {
   init() {
-    this.renderMovies(model.movies)
+    this.renderMovies(model.movies);
 
-    const form = document.querySelector('.form')
-    const inputTitle = document.querySelector('.input-title')
-    const inputDescription = document.querySelector('.input-description')
+    const form = document.querySelector(".form");
+    const inputTitle = document.querySelector(".input-title");
+    const inputDescription = document.querySelector(".input-description");
 
-    form.addEventListener('submit', function (event) {
-      event.preventDefault()
-      const title = inputTitle.value
-      const description = inputDescription.value
-      controller.addMovie(title, description)
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      const title = inputTitle.value;
+      const description = inputDescription.value;
+      controller.addMovie(title, description);
 
-      inputTitle.value = ''
-      inputDescription.value = ''
-    })
+      inputTitle.value = "";
+      inputDescription.value = "";
+    });
 
-    // your code
+    // your code_____________________________________________________________________
+    
   },
   renderMovies(movies) {
-    const list = document.querySelector('.list')
-    let moviesHTML = ''
+    const list = document.querySelector(".list");
+    let moviesHTML = "";
 
     for (const movie of movies) {
       moviesHTML += `
@@ -63,38 +64,38 @@ const view = {
           <p class="movie-description">${movie.description}</p>
           <button class="delete-button" type="button">Удалить 🗑</button>
         </li>
-      `
+      `;
     }
 
-    list.innerHTML = moviesHTML
+    list.innerHTML = moviesHTML;
   },
   displayMessage(message, isError = false) {
-    const messageBox = document.querySelector('.message-box')
-    messageBox.textContent = message
+    const messageBox = document.querySelector(".message-box");
+    messageBox.textContent = message;
     if (isError) {
-      messageBox.classList.remove('success')
-      messageBox.classList.add('error')
+      messageBox.classList.remove("success");
+      messageBox.classList.add("error");
     } else {
-      messageBox.classList.remove('error')
-      messageBox.classList.add('success')
+      messageBox.classList.remove("error");
+      messageBox.classList.add("success");
     }
   },
-}
+};
 
 const controller = {
   addMovie(title, description) {
-    if (title.trim() !== '' && description.trim() !== '') {
-      model.addMovie(title, description)
-      view.displayMessage('Фильм добавлен успешно!')
+    if (title.trim() !== "" && description.trim() !== "") {
+      model.addMovie(title, description);
+      view.displayMessage("Фильм добавлен успешно!");
     } else {
-      view.displayMessage('Заполните все поля!', true)
+      view.displayMessage("Заполните все поля!", true);
     }
   },
-  // your code
-}
+  // your code - здесь кнопка для удаления фильма
+};
 
 function init() {
-  view.init()
+  view.init();
 }
 
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener("DOMContentLoaded", init);
